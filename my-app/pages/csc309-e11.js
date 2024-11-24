24da257e3e38a0c386c15f60b532ee4bc576b3c2
bayazitbora <bayazitbora19@gmail.com>
2024-11-24 18:06:07 -0500

    Created next.js app

---
+++ my-app/pages/csc309-e11.js
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <div id="auto-tester">
+            Hi. I am bayazitb, and I have learned how to develop a full-stack web app
+            and deploy it!
+        </div>
+    );
+}
